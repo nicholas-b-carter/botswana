6c10512d7e491019d3087ebcc9026c1fb4b8c65e
ncarter <ncarter@madmobile.com>
2017-01-28 13:42:03 -0500

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Create bot and add dialogs
+var connector = new builder.ChatConnector({
+    appId: "YourAppId",
+    appPassword: "YourAppSecret"
+});
+var bot = new builder.UniversalBot(connector);
+bot.dialog('/', function (session) {
+    session.send('Hello World');
+});
+
+// Setup Restify Server
+var server = restify.createServer();
+server.post('/api/messages', connector.listen());
+server.listen(process.env.port || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
